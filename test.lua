b6a5f382b6b64de3eaafa66d1ea8a2b07229f330
wenjie.duan <19900429>
2019-03-22 16:43:09 +0800

    初始化

---
+++ test.lua
@@ -0,0 +1,55 @@
+-- print("hello world")
+
+-- function fact( n )
+-- 	if n == 0 then
+-- 		return 1
+-- 	else 
+-- 		return n * fact(n-1)
+-- 	end
+-- end
+
+-- print("enter a number:")
+-- a = io.read("*number")
+-- print(fact(a))
+
+
+
+-- function norm(x, y)
+-- 	local n2 = x^2 + y^2
+-- 	return math.sqrt(n2)
+-- end
+
+-- function twice(x)
+-- 	return 2*x
+-- end
+
+-- > dofile("lib1.lua")
+-- > n = norm(3.4, 1.0)
+-- > print(twice(n))
+
+--[[
+print(b)
+b = 10
+print(b)
+b = nil
+print(b)
+--]]
+
+
+-- print(type("hello world"))
+-- print(type(10.4*3))
+-- print(type(print))
+-- print(type(type))
+-- print(type(true))
+-- print(type(nil))
+-- print(type(type(X)))
+
+
+
+print(type(a))
+a = 10
+print(type(a))
+a = "a string!!"
+print(type(a))
+a = print
+a(type(a))
